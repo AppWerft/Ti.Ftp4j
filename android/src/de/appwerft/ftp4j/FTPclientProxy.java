310b1900a1f6c6e4d38d9e90ad75a97cff18fe23
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-03-04 13:17:28 +0100

    all callbacks

--- android/src/de/appwerft/ftp4j/FTPclientProxy.java
+++ android/src/de/appwerft/ftp4j/FTPclientProxy.java
@@ -8,97 +8,189 @@
  */
 package de.appwerft.ftp4j;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import it.sauronsoftware.ftp4j.FTPAbortedException;
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferException;
+import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPFile;
+import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
+import it.sauronsoftware.ftp4j.FTPListParseException;
+
 import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
-import org.appcelerator.titanium.TiC;
 import org.appcelerator.kroll.common.Log;
 import org.appcelerator.kroll.common.TiConfig;
-import org.appcelerator.titanium.util.TiConvert;
-import org.appcelerator.titanium.proxy.TiViewProxy;
-import org.appcelerator.titanium.view.TiCompositeLayout;
-import org.appcelerator.titanium.view.TiCompositeLayout.LayoutArrangement;
-import org.appcelerator.titanium.view.TiUIView;
+import org.appcelerator.titanium.TiC;
 
-import android.app.Activity;
+import android.os.AsyncTask;
 
+@Kroll.proxy(creatableInModule = Ftp4jModule.class)
+public class FTPclientProxy extends KrollProxy {
+	private static final String LCAT = "FTP4j";
+	private String host;
+	private String login = "anonymous";
+	private String password = "ftp4j";
+	private int port = 0;
 
-// This proxy can be created by calling Ftp4j.createExample({message: "hello world"})
-@Kroll.proxy(creatableInModule=Ftp4jModule.class)
-public class FTPclientProxy extends TiViewProxy
-{
-	// Standard Debugging variables
-	private static final String LCAT = "ExampleProxy";
-	private static final boolean DBG = TiConfig.LOGD;
+	private String path = "/";
+	private String file = null;
+	private FTPClient client = new FTPClient();
+	private KrollFunction onLoad;
+	private KrollFunction onError;
 
-	private class ExampleView extends TiUIView
-	{
-		public ExampleView(TiViewProxy proxy) {
-			super(proxy);
-			LayoutArrangement arrangement = LayoutArrangement.DEFAULT;
-
-			if (proxy.hasProperty(TiC.PROPERTY_LAYOUT)) {
-				String layoutProperty = TiConvert.toString(proxy.getProperty(TiC.PROPERTY_LAYOUT));
-				if (layoutProperty.equals(TiC.LAYOUT_HORIZONTAL)) {
-					arrangement = LayoutArrangement.HORIZONTAL;
-				} else if (layoutProperty.equals(TiC.LAYOUT_VERTICAL)) {
-					arrangement = LayoutArrangement.VERTICAL;
-				}
-			}
-			setNativeView(new TiCompositeLayout(proxy.getActivity(), arrangement));
-		}
-
-		@Override
-		public void processProperties(KrollDict d)
-		{
-			super.processProperties(d);
-		}
-	}
-
-
-	// Constructor
-	public FTPclientProxy()
-	{
+	public FTPclientProxy() {
 		super();
 	}
 
 	@Override
-	public TiUIView createView(Activity activity)
-	{
-		TiUIView view = new ExampleView(this);
-		view.getLayoutParams().autoFillsHeight = true;
-		view.getLayoutParams().autoFillsWidth = true;
-		return view;
+	public void handleCreationDict(KrollDict opts) {
+		super.handleCreationDict(opts);
+		if (opts.containsKeyAndNotNull(TiC.PROPERTY_ONLOAD)) {
+			Object o = opts.get(TiC.PROPERTY_ONLOAD);
+			if (o instanceof KrollFunction) {
+				onLoad = (KrollFunction) o;
+			}
+		}
+		if (opts.containsKeyAndNotNull(TiC.PROPERTY_ONERROR)) {
+			Object o = opts.get(TiC.PROPERTY_ONERROR);
+			if (o instanceof KrollFunction) {
+				onError = (KrollFunction) o;
+			}
+		}
+		if (opts.containsKeyAndNotNull(TiC.PROPERTY_URL)) {
+			try {
+				URL url = new URL(opts.getString(TiC.PROPERTY_URL));
+				host = url.getHost();
+				String user;
+				user = url.getUserInfo();
+				if (user != null && user.contains(":")) {
+					login = user.split(":")[0];
+					password = user.split(":")[1];
+				}
+				port = url.getPort();
+				path = url.getPath();
+				file = url.getFile();
+			} catch (MalformedURLException e) {
+				Log.e(LCAT,
+						"wrong format url " + opts.getString(TiC.PROPERTY_URL));
+				e.printStackTrace();
+				sendError("malformedIURL", 0);
+			}
+		}
+		(new FTPsessionConnect()).execute();
+
 	}
 
-	// Handle creation options
-	@Override
-	public void handleCreationDict(KrollDict options)
-	{
-		super.handleCreationDict(options);
-
-		if (options.containsKey("message")) {
-			Log.d(LCAT, "example created with message: " + options.get("message"));
+	@Kroll.method
+	public void disconnect() {
+		if (client != null) {
+			Thread t = new Thread() {
+				public void run() {
+					try {
+						client.disconnect(true);
+					} catch (IllegalStateException | IOException
+							| FTPIllegalReplyException | FTPException e) {
+						e.printStackTrace();
+						sendError(e.getMessage(), 0);
+					}
+				}
+			};
+			t.start();
 		}
 	}
 
-	// Methods
 	@Kroll.method
-	public void printMessage(String message)
-	{
-		Log.d(LCAT, "printing message: " + message);
+	public boolean isConnected() {
+		return client.isConnected();
 	}
 
+	private class FTPsessionConnect extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... params) {
+			KrollDict kd = new KrollDict();
+			try {
+				if (port != 0)
+					client.connect(host, port);
+				else
+					client.connect(host);
+			} catch (IllegalStateException | IOException
+					| FTPIllegalReplyException | FTPException e1) {
+				e1.printStackTrace();
+				sendError(e1.getMessage(), 0);
+			}
+			try {
+				client.login(login, password);
+			} catch (IllegalStateException | IOException
+					| FTPIllegalReplyException | FTPException e1) {
+				e1.printStackTrace();
+				sendError(e1.getMessage(), 0);
+			}
+			String[] list = null;
+			try {
+				list = client.listNames();
+			} catch (IllegalStateException | IOException
+					| FTPIllegalReplyException | FTPException
+					| FTPDataTransferException | FTPAbortedException
+					| FTPListParseException e1) {
+				sendError(e1.getMessage(), 0);
+				e1.printStackTrace();
+			}
+			try {
+				client.changeDirectory("." + path);
+			} catch (IllegalStateException | IOException
+					| FTPIllegalReplyException | FTPException e1) {
+				e1.printStackTrace();
+				sendError(e1.getMessage(), 0);
+			}
+			String[] fileList = null;
+			try {
+				try {
+					fileList = client.listNames();
+				} catch (IllegalStateException | IOException
+						| FTPIllegalReplyException | FTPException e1) {
+					e1.printStackTrace();
+				}
+			} catch (FTPDataTransferException | FTPAbortedException
+					| FTPListParseException e) {
+				e.printStackTrace();
+				sendError(e.getMessage(), 0);
+			}
 
-	@Kroll.getProperty @Kroll.method
-	public String getMessage()
-	{
-        return "Hello World from my module";
+			kd.put("host", client.getHost());
+			kd.put("username", client.getUsername());
+			try {
+				kd.put("currentDirectory", client.currentDirectory());
+			} catch (IllegalStateException | IOException
+					| FTPIllegalReplyException | FTPException e1) {
+				sendError(e1.getMessage(), 0);
+				e1.printStackTrace();
+			}
+			kd.put("fileNames", fileList);
+			if (hasListeners("onready")) {
+				fireEvent("onready", kd);
+			}
+			if (onLoad != null) {
+				onLoad.call(getKrollObject(), kd);
+			}
+
+			return "Executed";
+		}
+
 	}
 
-	@Kroll.setProperty @Kroll.method
-	public void setMessage(String message)
-	{
-	    Log.d(LCAT, "Tried setting module message to: " + message);
+	private void sendError(String message, int number) {
+		if (onError != null) {
+			KrollDict kd = new KrollDict();
+			kd.put("message", message);
+			kd.put("error", number);
+			onError.call(getKrollObject(), kd);
+		}
+
 	}
 }
